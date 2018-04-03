2db7d5a0e1a5d3d7c0ec3c6c62f6064c836b37b9
developing <magedabdelkhalik95@gmail.com>
2018-04-03 16:02:41 +0200

    database

--- app/src/main/java/com/trying/developing/firebasetesting/CreateTaskActivity.java
+++ app/src/main/java/com/trying/developing/firebasetesting/CreateTaskActivity.java
@@ -1,5 +1,6 @@
 package com.trying.developing.firebasetesting;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -43,18 +46,30 @@ public class CreateTaskActivity extends AppCompatActivity {
         user = mAuth.getCurrentUser();
 
         String current_id = user.getUid();
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("tasks").child(current_id);
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference();
+
+//
+//        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("tasks").child(current_id);
+//
+//        mUserdDatabase = FirebaseDatabase.getInstance().getReference().child("users");
 
         mUserdDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+
     }
 
     public void createTask(View view) {
-        final HashMap<String, String> taskMap = new HashMap<>();
-        // String ss=memberEmail.getText().toString();
-        taskMap.put("email", memberEmail.getText().toString());
-        taskMap.put("daskdeadline", deadlinetask.getText().toString());
-        taskMap.put("taskdescription", taskdescription.getText().toString());
-        taskMap.put("taskname", tasksname.getText().toString());
+//        final HashMap<String, String> taskMap = new HashMap<>();
+//        // String ss=memberEmail.getText().toString();
+//        taskMap.put("email", memberEmail.getText().toString());
+//        taskMap.put("daskdeadline", deadlinetask.getText().toString());
+//        taskMap.put("taskdescription", taskdescription.getText().toString());
+//        taskMap.put("taskname", tasksname.getText().toString());
+
+        final String mMemberEmail =memberEmail.getText().toString();
+        final String mTasksName =tasksname.getText().toString();
+        final String mTaskDsc=taskdescription.getText().toString();
+        final String mTaskDeadline=deadlinetask.getText().toString();
 
 
         final Query userQuery = mUserdDatabase.orderByChild("email").equalTo(memberEmail.getText().toString());
@@ -65,11 +80,44 @@ public class CreateTaskActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                for (final DataSnapshot child : dataSnapshot.getChildren()) {
 
-                    String id = child.getKey();
+                    final String id = child.getKey();
+
 
                     Toast.makeText(CreateTaskActivity.this, id, Toast.LENGTH_SHORT).show();
+
+
+                    mAuth = FirebaseAuth.getInstance();
+                    user = mAuth.getCurrentUser();
+
+                    final String current_id = user.getUid();
+
+
+                 //   mUserDatabase = FirebaseDatabase.getInstance().getReference().child("tasks").child(current_id).child(id);
+
+                  //  WriteNewTask(current_id,id,mMemberEmail,mTasksName,mTaskDsc,mTaskDeadline);
+
+                    final Tasks tasks=new Tasks(mMemberEmail,mTasksName,mTaskDsc,mTaskDeadline);
+
+                    mUserDatabase.child("tasks").child(current_id).child(id).setValue(tasks).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            mAuth = FirebaseAuth.getInstance();
+                            user = mAuth.getCurrentUser();
+
+                            String current_id = user.getUid();
+
+                            Tasks tasks=new Tasks(mMemberEmail,mTasksName,mTaskDsc,mTaskDeadline);
+
+                            String id = child.getKey();
+
+                            mUserDatabase.child("tasks").child(id).child(current_id).setValue(tasks);
+
+                        }
+                    });
+
                 }
             }
 
@@ -79,9 +127,34 @@ public class CreateTaskActivity extends AppCompatActivity {
             }
         });
 
-        mUserDatabase.setValue(taskMap);
+
+
+//        mUserDatabase.setValue(taskMap);
 
 
     }
+
+
+//    public void WriteNewTask(String current_id,String member_id,String memberEmail,String taskname,String taskDsc,String taskDeadline){
+//
+//
+//        Tasks tasks=new Tasks(memberEmail,taskname,taskDsc,taskDeadline);
+//
+//        mUserDatabase.child("tasks").child(current_id).child(member_id).setValue(tasks).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                MemberReciveTask("member","id","email","taskname","task","deadline");
+//            }
+//        });
+//
+//    }
+//
+//    public void MemberReciveTask(String member_id,String current_id,String memberEmail,String taskname,String taskDsc,String taskDeadline){
+//
+//        Tasks tasks=new Tasks(memberEmail,taskname,taskDsc,taskDeadline);
+//
+//        mUserDatabase.child("tasks").child(member_id).child(current_id).setValue(tasks);
+//
+//    }
 
 }
---
+++ app/src/main/java/com/trying/developing/firebasetesting/Tasks.java
@@ -0,0 +1,53 @@
+package com.trying.developing.firebasetesting;
+
+/**
+ * Created by developing on 4/3/2018.
+ */
+
+public class Tasks {
+
+    private String mMemberEmail;
+    private String mTaskname;
+    private String mTaskDsc;
+    private String mTaskDeadline;
+
+
+    public Tasks(String mMemberEmail, String mTaskname, String mTaskDsc, String mTaskDeadline) {
+        this.mMemberEmail = mMemberEmail;
+        this.mTaskname = mTaskname;
+        this.mTaskDsc = mTaskDsc;
+        this.mTaskDeadline = mTaskDeadline;
+    }
+
+    public String getmMemberEmail() {
+        return mMemberEmail;
+    }
+
+    public void setmMemberEmail(String mMemberEmail) {
+        this.mMemberEmail = mMemberEmail;
+    }
+
+    public String getmTaskname() {
+        return mTaskname;
+    }
+
+    public void setmTaskname(String mTaskname) {
+        this.mTaskname = mTaskname;
+    }
+
+    public String getmTaskDsc() {
+        return mTaskDsc;
+    }
+
+    public void setmTaskDsc(String mTaskDsc) {
+        this.mTaskDsc = mTaskDsc;
+    }
+
+    public String getmTaskDeadline() {
+        return mTaskDeadline;
+    }
+
+    public void setmTaskDeadline(String mTaskDeadline) {
+        this.mTaskDeadline = mTaskDeadline;
+    }
+}
